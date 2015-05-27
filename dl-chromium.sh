2e771b31fd45fb44f932e624acee3012c45d03ae
Alex Normand <normand@adobe.com>
2015-05-27 10:18:24 +0200

    pipe zip file to bsdtar (bsdtar can read zip archives :)) instead of creating a temporary zip file and removing it immediately'

--- dl-chromium.sh
+++ dl-chromium.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-curl -#L https://download-chromium.appspot.com/dl/Mac > chrome-mac.zip
-unzip chrome-mac.zip
-cp -fr chrome-mac /Applications
-rm -fr chrome-mac chrome-mac.zip
-
+curl -#L https://download-chromium.appspot.com/dl/Mac | bsdtar -xvf- -C /Applications
